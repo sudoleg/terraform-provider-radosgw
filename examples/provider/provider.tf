9a7f7817be958817920043d96dc41cb925d816ae
Luna Stadler <luc@spreadshirt.net>
2023-07-04 11:18:15 +0200

    Remove unused code from provider example
    This disappeared with a restart of the docker compose setup, which is a
    bit unfortunate, but "just the way it is" for now.

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -11,19 +11,6 @@ provider "radosgw" {
   # set access_key_id and secret_access_key via ACCESS_KEY_ID and SECRET_ACCESS_KEY env variables
 }
 
-data "radosgw_buckets" "buckets" {
-}
-
-# resource "radosgw_user" "terraformed_user" {
-#   user_id      = "terraformed"
-#   display_name = "User created by terraform for testing (modified)"
-#   keys = [
-#     {
-#       user = "terraformed"
-#     }
-#   ]
-# }
-
 resource "radosgw_user" "demo_user" {
   user_id      = "demo"
   display_name = "Ceph demo user"
